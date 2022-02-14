2d3f1eed1a1c08c1d60c4b76738e569c010e6125
Demorri <98537439+Demorri@users.noreply.github.com>
2022-02-14 21:00:16 +0200

    Update Main.java

--- N8/src/com/company/Main.java
+++ N8/src/com/company/Main.java
@@ -6,17 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int x1, x2, y1, y2;
-
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter first 2 coordinates: ");
-        x1 = in.nextInt();
-        y1 = in.nextInt();
+        var x1 = in.nextInt();
+        var y1 = in.nextInt();
 
         System.out.println("Enter second 2 coordinates: ");
-        x2 = in.nextInt();
-        y2 = in.nextInt();
+        var x2 = in.nextInt();
+        var y2 = in.nextInt();
 
         if(x1 == x2 || y1 == y2 || Math.abs(x1-y1) == Math.abs(x2-y2)){
             System.out.print("YES");
